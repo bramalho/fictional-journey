7b3830a48dab96857ba535ea3182fd4e64f4afe6
Bruno Ramalho <bramalho@divante.pl>
2020-10-09 12:32:43 +0200

    documents

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
---
+++ api/src/Document/Product.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+/**
+ * @MongoDB\Document
+ */
+class Product
+{
+    /**
+     * @MongoDB\Id
+     */
+    public $id;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    public $uid;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    public $name;
+
+    /**
+     * @MongoDB\Field(type="integer")
+     */
+    public $price;
+
+    /**
+     * @MongoDB\ReferenceOne(targetDocument="App\Document\Category")
+     */
+    public $category;
+}
